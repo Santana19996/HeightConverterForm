485c900b7ad8a984e22701b6f1c680fd989e0b92
Oscar <oscarsantana2014@gmail.com>
2021-10-28 22:19:37 -0500

    Created first Jform through intellij GUI

---
+++ src/main/java/ConversionForm.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class ConversionForm {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton convertButton;
+    private JTextField textField3;
+    private JButton clearButton;
+    private JButton exitButton;
+}
